209c61a40e57977d9bdae82964a0621d47acf4f2
tokiv <tokiv515@gmail.com>
2022-08-05 14:25:17 +0200

    id updated

---
+++ EXU/id.vhd
@@ -0,0 +1,35 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_arith.all;
+use work.riscv_pkg.all;
+
+entity id is
+  port (
+    pc, instr: IN std_logic_vector(REG_WIDTH - 1 downto 0);
+    op_out: OUT opcode_t
+  ) ;
+end id;
+
+architecture behave of id is
+
+    signal op: opcode_t;
+    signal imm: std_logic_vector(31 downto 0);
+
+begin
+    op <= pc(6 downto 0);
+
+    funct7 <= pc(31 downto 25);
+    rs2 <= pc(24 downto 20);
+    rs1 <= pc(19 downto 14);
+    funct3 <= pc(13 downto 11);
+    rd <= pc(11 downto 7);
+
+    imm <= conv_std_logic_vector(20, 0) & pc(31 downto 20) when (op = LXX) or (op = ARII) or (op = FEN) or (op = CSR) or (op = JALR) -- i type
+        else conv_std_logic_vector(20, 0) & pc(31 downto 25) & pc(11 downto 7) when op = SXX -- s type
+        else conv_std_logic_vector(19, 0) & pc(31) & pc(7) & pc(30 downto 25) & pc(11 downto 8) & '0' when op = BXX -- b type
+        else pc(31 downto 12) & conv_std_logic_vector(12, 0) when (op = LUI) or (op = AUI) -- u type
+        else conv_std_logic_vector(11, 0) & pc(31) & pc(19 downto 12) & pc(20) & pc(30 downto 21) & '0' when op = JAL -- j type
+        else (others => '0'); 
+
+    op_out <= op;
+end behave ; -- behave
--- riscv_pkg.vhd
+++ riscv_pkg.vhd
@@ -7,11 +7,22 @@ package riscv_pkg is
     constant REG_WIDTH: integer := 32;
     constant MEM_DEPTH: integer := 1024;
     constant RF_ADDR_WIDTH: integer := 5;
+    constant RF_AMOUNT: integer := 2**RF_ADDR_WIDTH;
 
     constant NOP: std_logic_vector(REG_WIDTH - 1 downto 0) := conv_std_logic_vector(19, REG_WIDTH); -- addi r0 r0 0
     subtype opcode_t is std_logic_vector(6 downto 0);
     type mem_t is array (MEM_DEPTH - 1 downto 0) of std_logic_vector(REG_WIDTH - 1 downto 0);
-    constant JAL: opcode_t := "1101111";
+
+    -- opcodes
+    constant LUI : opcode_t := "0110111";
+    constant AUI : opcode_t := "0010111"; -- auipc
+    constant JAL : opcode_t := "1101111";
     constant JALR: opcode_t := "1100111";
-    constant BXX: opcode_t := "1100011";
+    constant BXX : opcode_t := "1100011";
+    constant LXX : opcode_t := "0000011";
+    constant SXX : opcode_t := "0100011";
+    constant ARII: opcode_t := "0010011"; -- arith with immediate
+    constant ARI : opcode_t := "0110011"; -- arith with registers
+    constant FEN : opcode_t := "0001111"; -- fence, fencei
+    constant CSR : opcode_t := "1110011"; -- exception, csr
 end package ;
